d0d45e56cc170039aa2d089151e0c2d0f6d1d642
Ayahm ibrahim <ayhamfacebook1999@gmail.com>
2024-01-17 20:26:48 +0300

    Comment and Reply model and migration

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -2,9 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Comment;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Blog extends Model
 {
@@ -24,5 +25,10 @@ class Blog extends Model
     public function tags()
     {
         return $this->belongsToMany(Tag::class, 'tags_blogs');
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
     }
 }
---
+++ app/Models/Reply.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\User;
+use App\Models\Comment;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Reply extends Model
+{
+    use HasFactory,SoftDeletes;
+
+    protected $fillable = [
+        "user_id",
+        "comment_id",
+        "body",
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function comment()
+    {
+        return $this->belongsTo(Comment::class, 'user_id');
+    }
+
+}
